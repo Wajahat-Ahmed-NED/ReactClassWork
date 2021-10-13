ae0ba5ed56f7b08dfa4c798606fa5545691c9074
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-10-13 13:26:50 +0500

    practicing react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  // react now uses function based components not class based components because it is easy for use
   const [inpValue,setInpVal]=useState('waji')
   let a="Almeer munna"
   let b=[1,2,3,5,4,6,7,8,0]
@@ -23,8 +24,10 @@ function App() {
         return <li key={index}>{item} Index={index}</li>
       })
     }</ul>
+    
     </div>
   );
 }
+// jsx ka faida ye ha ke hm is ke andar js ko use krsktey han
 
 export default App;
